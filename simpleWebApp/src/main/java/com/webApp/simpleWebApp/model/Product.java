88994f9cec8fd5b59fe6c627cf9f257d7b95e73a
aakash0101200 <aakashkumargupta.cse27@jecrc.ac.in>
2025-04-11 10:26:44 +0530

    lec. 16 -17

--- simpleWebApp/src/main/java/com/webApp/simpleWebApp/model/Product.java
+++ simpleWebApp/src/main/java/com/webApp/simpleWebApp/model/Product.java
@@ -1,6 +1,8 @@
 package com.webApp.simpleWebApp.model;
 
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,9 +12,11 @@ import org.springframework.stereotype.Component;
 @AllArgsConstructor
 @NoArgsConstructor
 @Component
+@Entity
 
 public class Product {
 
+    @Id
     private int prodId;
     private String prodName;
     private int price;
