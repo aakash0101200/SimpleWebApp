0588984af7c5a43b55a38bca7763ee6efa70b485
aakash0101200 <aakashkumargupta.cse27@jecrc.ac.in>
2025-04-11 21:17:40 +0530

    lec. 16 -17

--- simpleWebApp/src/main/java/com/webApp/simpleWebApp/service/ProductService.java
+++ simpleWebApp/src/main/java/com/webApp/simpleWebApp/service/ProductService.java
@@ -17,11 +17,6 @@ public class ProductService {
     ProductRepo repo;
 
 
-//    List<Product> products =  new ArrayList<>(Arrays.asList(
-//            new Product(101, "Iphone", 50000),
-//            new Product(102, "Canon Camera", 70000),
-//            new Product(103, "Shure Mic", 10000)));
-
     public List<Product> getProducts() {
 
         return repo.findAll();
